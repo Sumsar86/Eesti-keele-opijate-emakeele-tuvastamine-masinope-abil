6c1b86bac1b9dc72d45dd6b322fb53579c32ccd9
Sumsar <32791295+Sumsar86@users.noreply.github.com>
2021-04-02 20:54:57 +0300

    Lisasin "Andmestike sisselaadimine" ja "koik_dokumendid"

---
+++ Andmete sisselaadimine.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import glob
+
+
+def andmed():
+
+    # Andmete (teksti kood, autori emakeel ja tekstikeel) internetist alla laadimine ja Pandase
+    # DataFrame objekti panemine ning üleliigsete veergude eemaldamine.
+
+    url = "http://www.tlu.ee/~jaagup/andmed/keel/korpus/dokmeta.txt"
+
+    names = [
+        "kood",
+        "korpus",
+        "tekstikeel",
+        "tekstityyp",
+        "elukoht",
+        "taust",
+        "vanus",
+        "sugu",
+        "emakeel",
+        "kodukeel",
+        "keeletase",
+        "haridus",
+        "abivahendid",
+    ]
+
+    dataset = pd.read_csv(url, names=names)
+
+    dataset = dataset.drop(dataset.columns[[1, 3, 4, 5, 6, 7, 9, 10, 11, 12]], axis=1)
+    dataset = dataset.drop(dataset.index[0])
+
+    dataset = dataset.astype({"kood": str, "emakeel": str, "tekstikeel": str})
+
+    dataset = dataset[
+        (dataset["emakeel"] == "eesti")
+        | (dataset["emakeel"] == "soome")
+        | (dataset["emakeel"] == "vene")
+    ]
+    dataset = dataset[dataset["tekstikeel"] == "eesti"]
+
+    max_pikkus = min(
+        len(dataset[dataset["emakeel"] == "eesti"]),
+        len(dataset[dataset["emakeel"] == "soome"]),
+        len(dataset[dataset["emakeel"] == "vene"]),
+    )
+
+    ds = pd.DataFrame()
+    ds = ds.append(dataset[dataset["emakeel"] == "eesti"].sample(max_pikkus))
+    ds = ds.append(dataset[dataset["emakeel"] == "soome"].sample(max_pikkus))
+    ds = ds.append(dataset[dataset["emakeel"] == "vene"].sample(max_pikkus))
+    ds = ds.reset_index()
+    del ds["index"]
+
+    # Tekstid on juba alla laeutd ning asuvad kasutas "koik_dokumendid", kust neid ükshaaval loetakse,
+    # seotakse koodi järgi eelnevate andmetega ja pannakse edasiseks kasutamiseks uude DataFrame objekti.
+
+    tekstid = []
+    oiged_margid = (
+        "q",
+        "w",
+        "e",
+        "r",
+        "t",
+        "y",
+        "u",
+        "i",
+        "o",
+        "p",
+        "ü",
+        "õ",
+        "a",
+        "s",
+        "d",
+        "f",
+        "g",
+        "h",
+        "j",
+        "k",
+        "l",
+        "ö",
+        "ä",
+        "z",
+        "x",
+        "c",
+        "v",
+        "b",
+        "n",
+        "m",
+        "ž",
+        "š",
+        " ",
+    )
+    failid = glob.glob(r"koik_dokumendid\*.txt")
+
+    # Tekstide töötlemine. Haruldased või tavapäratud ASCII märgid eemaldatakse tekstidest.
+
+    for i in range(len(failid)):
+        if failid[i][-25:-4] in set(ds["kood"]):
+            f = open(failid[i], "r")
+            x = f.read().replace("\n", " ")
+            for y in x:
+                if y.lower() in oiged_margid:
+                    x.replace(y, "")
+            if x != "":
+                tekstid.append([x, failid[i][-25:-4]])
+            f.close()
+
+    toodeldud_failid = pd.DataFrame(tekstid, columns=["tekst", "kood"])
+    uus_dataset = pd.merge(dataset, toodeldud_failid, on="kood")
+    uus_dataset = uus_dataset.drop(uus_dataset.columns[[1]], axis=1)
+
+    return uus_dataset
+
+
+if __name__ == "__main__":
+    ds = andmed()
+
+    print("Andmestiku veerud:")
+    [print("   ", i) for i in ds.columns]
+    print("\nAndmestiku ridade/tunnuste arv:", len(ds))
+    print("\nAndmestik:")
+    print(ds.head)
---
+++ koik_dokumendid/README.txt
@@ -0,0 +1,8 @@
+Kõik tekstifailid tuleb allalaadida ja panna siia kausta. Selleks tuleb: 
+
+	1. minna leheküljele "http://www.tlu.ee/~jaagup/andmed/keel/korpus/",
+	2. sealt allalaadida fail "dokumendid.zip",
+	3. tõsta allalaetud "dokumendid.zip" fail siia kausta (koik_dokumendid), 
+	4. avada see, vajutades selle peal paremat hiireklahvi ja valides "Ekstrakti kõik...", 
+	5. kustutada "dokumendid.zip" ja "README.txt",
+	6. oletegi valmis.
